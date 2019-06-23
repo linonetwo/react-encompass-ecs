f614f59320dcfcb603743dbf4fe5b805ecf19778
linonetwo <linonetwo012@gmail.com>
2019-06-23 22:41:45 +0800

    wip: consider serialize component's name, and move whole ECS calculation to worker

--- src/entity.tsx
+++ src/entity.tsx
@@ -16,7 +16,7 @@ export interface IEntityResultMap {
   };
 }
 
-export const GameEntitiesContext = createContext<IEntityMap>({});
+export const GameStoreContext = createContext<IEntityMap>({});
 
 /**
  * Select entity with provided components, return components inside entity
@@ -31,7 +31,7 @@ export const GameEntitiesContext = createContext<IEntityMap>({});
 export function useComponent<TComponent extends Component, T extends Type<TComponent> /* | Array<Type<TComponent>> */>(
   descriptions: { [name: string]: /* T[] */ Array<Type<TComponent>> },
   forceRender: boolean = false,
-  context: Context<IEntityMap> = GameEntitiesContext,
+  context: Context<IEntityMap> = GameStoreContext,
 ): {
   [name: string]: Array<
     Array</* T extends Array<Type<TComponent>> ? GCOptimizedList<Readonly<TComponent>> : */ Readonly<TComponent>>
@@ -39,51 +39,15 @@ export function useComponent<TComponent extends Component, T extends Type<TCompo
 } {
   // prevent object passed in trigger reRender, assume that description won't change on runtime
   const [selectedEntities, setter] = useImmer<IEntityResultMap>({});
-  const entities = useContext(context);
-  useEffect(() => {
-    // select entities that match the components description
-    setter(draft => {
-      for (const name of Object.keys(descriptions)) {
-        for (const entity of Object.values(entities)) {
-          const id = entity.get_component(ReactSyncComponent).entity_id;
-          const components = descriptions[name];
-          if (draft[name] === undefined) {
-            draft[name] = {};
-          }
-          if (draft[name][id] !== entity && EntityChecker.check_entity(entity, flatten(components))) {
-            draft[name][id] = entity;
-          }
-        }
-      }
-    });
-    // only rerun this selection if entities changes
-  }, [setter, entities]);
-  const selectedComponents = useMemo(() => {
-    const result = mapValues(selectedEntities, (entitiesWithSuchComponent, name) =>
-      Object.values(entitiesWithSuchComponent).map(entity =>
-        descriptions[name].map(component => {
-          // if (Array.isArray(component)) {
-          //   component as Array<Type<TComponent>>;
-          //   return entity.get_components(component[0]);
-          // }
-          return entity.get_component(component);
-        }),
-      ),
-    );
-    // assign empty array, so you can xxx.map it easily in React
-    for (const name of Object.keys(descriptions)) {
-      if (!result[name]) {
-        result[name] = [];
-      }
-    }
-    return result;
-  }, [selectedEntities]);
+  const store = useContext(context);
   useUpdate(forceRender);
 
+  // TODO: call async version of EntitySyncer, or its warper here
   return selectedComponents;
 }
 
+// TODO: pass down EntitySyncer, or its warper
 export function Provider(props: { children: React.ReactNode; entities: IEntityMap; context?: Context<IEntityMap> }) {
-  const ProvidedContext = props.context || GameEntitiesContext;
+  const ProvidedContext = props.context || GameStoreContext;
   return <ProvidedContext.Provider value={props.entities}>{props.children}</ProvidedContext.Provider>;
 }
--- src/sync.ts
+++ src/sync.ts
@@ -1,8 +1,23 @@
 import { Subject } from 'rxjs';
-import { Renders, EntityRenderer, GeneralRenderer, Entity, WorldBuilder } from 'encompass-ecs';
-import { produce } from 'immer';
+import {
+  Renders,
+  EntityRenderer,
+  GeneralRenderer,
+  Entity,
+  WorldBuilder,
+  Component,
+  Type,
+  EntityChecker,
+} from 'encompass-ecs';
+import { flatten, mapValues } from 'lodash';
 import { ReactSyncComponent } from './component';
 import { IEntityMap } from './entity';
+
+export interface IEntityResultMap {
+  [name: string]: {
+    [id: number]: Entity;
+  };
+}
 
 export const updaterContext: Subject<boolean> = new Subject();
 export class EntitySyncer {
@@ -12,10 +27,8 @@ export class EntitySyncer {
   private currentUpdaterValue: boolean = true;
 
   constructor(worldBuilder: WorldBuilder) {
+    // TODO: use weak map instead
     this.entities = {};
-    const updateEntitiesStore = produce((draft, id, newEntity) => {
-      draft[id] = newEntity;
-    });
     const store = this; // tslint:disable-line no-this-assignment pass this from this class to temporary classes below
 
     @Renders(ReactSyncComponent)
@@ -24,7 +37,7 @@ export class EntitySyncer {
       public render(currentEntity: Entity) {
         // put current entity into the resulting entity map, update the old one
         const id = currentEntity.get_component(ReactSyncComponent).entity_id;
-        store.entities = updateEntitiesStore(store.entities, id, currentEntity);
+        store.entities[id] = currentEntity;
       }
     }
     worldBuilder.add_renderer(ReactSyncRenderer);
@@ -38,5 +51,50 @@ export class EntitySyncer {
       }
     }
     worldBuilder.add_renderer(ReactReRenderTriggerRenderer);
+  }
+
+  public useComponent<TComponent extends Component, T extends Type<TComponent> /* | Array<Type<TComponent>> */>(
+    descriptions: { [name: string]: /* T[] */ Array<Type<TComponent>> },
+    forceRender: boolean = false,
+  ): {
+    [name: string]: Array<
+      Array</* T extends Array<Type<TComponent>> ? GCOptimizedList<Readonly<TComponent>> : */ Readonly<TComponent>>
+    >;
+  } {
+    // select entities that match the components description
+    // TODO: extract this to a memorized function, invalided when "entities" changes
+    const selectedEntities: IEntityResultMap = {};
+    for (const name of Object.keys(descriptions)) {
+      for (const entity of Object.values(this.entities)) {
+        const id = entity.get_component(ReactSyncComponent).entity_id;
+        const components = descriptions[name];
+        if (selectedEntities[name] === undefined) {
+          selectedEntities[name] = {};
+        }
+        if (selectedEntities[name][id] !== entity && EntityChecker.check_entity(entity, flatten(components))) {
+          selectedEntities[name][id] = entity;
+        }
+      }
+    }
+    // TODO: extract this to a memorized function, invalided when "selectedEntities" changes
+    const selectedComponents = mapValues(selectedEntities, (entitiesWithSuchComponent, name) =>
+      Object.values(entitiesWithSuchComponent).map(entity =>
+        descriptions[name].map(component => {
+          // if (Array.isArray(component)) {
+          //   component as Array<Type<TComponent>>;
+          //   return entity.get_components(component[0]);
+          // }
+          return entity.get_component(component);
+        }),
+      ),
+    );
+    // assign empty array, so you can xxx.map it easily in React
+    for (const name of Object.keys(descriptions)) {
+      if (!selectedComponents[name]) {
+        selectedComponents[name] = [];
+      }
+    }
+
+    return selectedComponents;
   }
 }
